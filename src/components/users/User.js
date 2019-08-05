c4ba392a92dd47c1f31f0acc04a625cf86cdd6ad
Hasan Aydin <hasanaydins@gmail.com>
2019-08-06 02:10:53 +0300

    styled

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+
 
 export class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login)
     }
     static propTypes = {
-        loading: PropTypes.bool,
+        loading: PropTypes.bool.isRequired,
         user: PropTypes.object.isRequired,
         getUser: PropTypes.func.isRequired
     }
@@ -17,6 +19,7 @@ export class User extends Component {
             avatar_url,
             location,
             bio,
+            company,
             blog,
             login,
             html_url,
@@ -26,14 +29,75 @@ export class User extends Component {
             public_gists,
             hireable
         } = this.props.user;
-        const {loading} = this.props;
-        if(loading){return <Spinner></Spinner>} 
-        return (
-            <div>
-                {name}
-            </div>
-        )
-    }
-}
 
+        const {loading} = this.props;
+        
+        if(loading) {
+            return <Spinner/>       
+        } 
+
+        return <Fragment>
+            <Link to='/' className='btn btn-secondary mt-1'> &lt; Back to Home</Link>
+            <h5 className="mt-4">Hireable:  
+            {hireable ? <i className="fas fa-check text-success mt-2 ml-2" /> : <i className="fas fa-times-circle text-danger mt-2 ml-2" /> }</h5> 
+
+            <div className="row">
+            <div className="card col-12 col-md-4 " style={userStyle}>
+                <div className="text-center">
+                    <img src={avatar_url} className="rounded-circle mt-3" style={{width: '150px'}} />
+                    <h1>{name}</h1>
+                    <p>@{login}</p>
+                    <p>Location: {location}</p>
+                </div>
+                <div>
+                    {bio && (<Fragment>
+                         <h3>Bio</h3>
+                         <p>{bio}</p>
+                    </Fragment>
+                    )}
+                    <a href={html_url} className="btn btn-dark my-1 mb-2">Visit Github Profile</a>
+                    <ul>
+                        <li>
+                            {company && <Fragment>
+                                <strong>Company: </strong> {company}
+                            </Fragment>}
+                        </li>
+                        <li>
+                        {blog && <Fragment>
+                                <strong>Website: </strong> {blog}
+                            </Fragment>}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+             <div className="col-12 col-md-8">
+             
+                <div className="card mr-2">
+                <div className="card-body text-center" style={userStyle}>
+
+             <button type="button" className="btn btn-info">
+             Followers: <span className="badge badge-light">{followers}</span>
+             </button>
+             <button type="button" className="btn btn-success ml-2">
+             Following: <span className="badge badge-light">{following}</span>
+             </button>
+             <button type="button" className="btn btn-dark ml-2">
+             Public Repos: <span className="badge badge-light">{public_repos}</span>
+             </button>
+             <button type="button" className="btn btn-danger ml-2  ">
+             Public Gists: <span className="badge badge-light">{public_gists}</span>
+             </button>
+
+             </div>
+             </div>
+             </div>                
+            </div>
+        </Fragment>;
+        
+    };
+}
+const userStyle = {
+    boxShadow: '0 8px 12px 0 rgba(87, 87, 87, 0.17)',
+    backgroundColor: 'rgb(239, 239, 239)'
+}
 export default User
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
 
 
 const UserItem = ({user: {login, avatar_url, html_url}}) => {
 
-        return (
+        return ( <Card>
                 <div className="card text-center justify-content-center mt-4">
-                <img src={avatar_url} alt="" className="rounded-circle mx-auto mt-3 mb-3" style={{width: '60px' }} />
+                <img src={avatar_url} alt="" className="rounded-circle border mx-auto mt-3 mb-3" style={{width: '70px' }} />
                 <h3>{login}</h3>
                 <div>
-                    <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">More</Link>
+                    <Link to={`/user/${login}`}  className="btn btn-dark btn-sm my-1 mb-3">More</Link>
                     
                 </div>
             </div>
+        </Card>
+                
 
         );
 };
@@ -21,5 +24,17 @@ const UserItem = ({user: {login, avatar_url, html_url}}) => {
 UserItem.propTypes ={
     user: PropTypes.object.isRequired,
 };
+const Card = styled.div`
+
+  .card:hover {
+    box-shadow: 0 5px 16px 0 rgba(86, 54, 255, 0.2 );
+
+ },
+ &.card{
+    box-shadow: 0 5px 16px 0 rgba(86, 54, 255, 0.1 );
+    background-color: rgba(223, 223, 223, 0.95);
+
+}
+`;
 
 export default UserItem;
